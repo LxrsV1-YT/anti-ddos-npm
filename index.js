f55f2252181d736199c0586549e5e8acce831764
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2021-01-04 19:58:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+exports.memory = function(){
+    require("./exports/memory.js")
+}
+exports.ping = function(){
+    require("./exports/ping.js")
+}
+exports.ddosCheck = function(){
+    require("./exports/ddoscheck.js")
+}
+exports.checkInterval = function(){
+    require("./exports/checkinterval.js")
+}
+// Please make sure to read
+// README.md
